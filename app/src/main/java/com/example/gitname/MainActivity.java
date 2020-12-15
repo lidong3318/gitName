f8370fb4c466a4295c547804033e43f2d17374c8
lidong3318 <331824732@qq.com>
2020-12-15 16:23:55 +0800

    第一次提交代码

--- app/src/main/java/com/example/gitname/MainActivity.java
+++ app/src/main/java/com/example/gitname/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.gitname;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        img = (ImageView) findViewById(R.id.img);
+
     }
 }
